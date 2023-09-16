2cd571f73b931df4d84b01bdd73f3f2be458a1cc
NriotHrreion <nriot233@outlook.com>
2023-09-16 19:16:31 +0800

    Delete bundled test files

--- dist/cjs/types/src/Toggle.d.ts
+++
@@ -1,7 +0,0 @@
-import React from "react";
-interface ToggleProps {
-    defaultToggleValue?: boolean;
-    onToggle?: (isActive: boolean) => void;
-}
-declare const Toggle: React.FC<ToggleProps & React.HTMLAttributes<HTMLDivElement>>;
-export default Toggle;
--- rollup.config.js
+++ rollup.config.js
@@ -36,7 +36,7 @@ module.exports = [
         ],
     },
     {
-        input: "dist/esm/types/src/index.d.ts",
+        input: "dist/esm/types/index.d.ts",
         output: [{ file: "dist/index.d.ts", format: "esm" }],
         plugins: [dts()],
     },
